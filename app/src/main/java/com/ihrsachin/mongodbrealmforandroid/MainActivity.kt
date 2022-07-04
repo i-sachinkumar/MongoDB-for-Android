c1cdb50b6b8359d104287517f7af383c59e1cab0
Sachin Kumar <81405044+i-sachinkumar@users.noreply.github.com>
2022-07-04 07:46:25 +0530

    Update MainActivity.kt

--- app/src/main/java/com/ihrsachin/mongodbrealmforandroid/MainActivity.kt
+++ app/src/main/java/com/ihrsachin/mongodbrealmforandroid/MainActivity.kt
@@ -34,7 +34,7 @@ import org.bson.Document
 class MainActivity : AppCompatActivity() {
 
     //unique app id
-    private var appId = "application-0-wjyfy"
+    private var appId = //TODO("Enter Your App Id")
 
 
     //MongoDb fields
